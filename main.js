59e4215421a42f402d99a899450474a93a28934f
codeme254 <otwomadennis365@gmail.com>
2023-11-20 06:23:42 -0600

    Change background of objectives section
    Change the background of the objectives section and add close
    functionality to the links at responsive mode to close the nav when they
    scroll to the desired section

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const headerNav = document.getElementById("header__nav");
 const headerButton = document.getElementById("header__button");
+const headerNavLinks = document.querySelectorAll(".header__nav--link");
 
-headerButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  headerNav.classList.toggle("nav_closed");
-
+const changeMenuIcon = () => {
   if (headerNav.classList.contains("nav_closed")) {
     headerButton.innerHTML = `<svg>
         <use xlink:href="sprite.svg#icon-menu"></use>
@@ -14,4 +12,25 @@ headerButton.addEventListener("click", (e) => {
         <use xlink:href="sprite.svg#icon-cross"></use>
       </svg>`;
   }
+};
+
+headerButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  headerNav.classList.toggle("nav_closed");
+  changeMenuIcon();
+});
+
+headerNavLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    headerNav.classList.add("nav_closed");
+    const targetId = link.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+      targetSection.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+    changeMenuIcon();
+  });
 });
