2a67cbbc01579adb80449d05fbf5a4c164174a7e
codeme254 <otwomadennis365@gmail.com>
2023-11-28 23:41:14 -0600

    disable changing menu icon

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ const headerNav = document.getElementById("header__nav");
 const headerButton = document.getElementById("header__button");
 const headerNavLinks = document.querySelectorAll(".header__nav--link");
 
-const changeMenuIcon = () => {
-  if (headerNav.classList.contains("nav_closed")) {
-    headerButton.innerHTML = `<svg>
-        <use xlink:href="sprite.svg#icon-menu"></use>
-      </svg>`;
-  } else {
-    headerButton.innerHTML = `<svg>
-        <use xlink:href="sprite.svg#icon-cross"></use>
-      </svg>`;
-  }
-};
+// const changeMenuIcon = () => {
+//   if (headerNav.classList.contains("nav_closed")) {
+//     headerButton.innerHTML = `<svg>
+//         <use xlink:href="sprite.svg#icon-menu"></use>
+//       </svg>`;
+//   } else {
+//     headerButton.innerHTML = `<svg>
+//         <use xlink:href="sprite.svg#icon-cross"></use>
+//       </svg>`;
+//   }
+// };
 
 const toggleNav = (e) => {
   e.preventDefault();
