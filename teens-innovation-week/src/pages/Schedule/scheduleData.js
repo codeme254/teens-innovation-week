4abc4031244518880a44aca4331183e407164113
codeme254 <otwomadennis365@gmail.com>
2023-12-02 13:04:37 +0300

    prepare data for day 1

---
+++ teens-innovation-week/src/pages/Schedule/scheduleData.js
@@ -0,0 +1,82 @@
+export const day1 = [
+    {
+        event: "Opening Remarks",
+        description: "Open the event",
+        guests: ["Felix Malombe", "Afandi Indiatsi"],
+        duration: "10",
+        start: "9:00:00 AM",
+        end: "9:10:00 AM"
+    },
+    {
+        event: "Opening Remarks",
+        description: "Open Address",
+        guests: ["Woodcreek School"],
+        duration: "10",
+        start: "9:10:00 AM",
+        end: "9:20:00 AM"
+    },
+    {
+        event: "Ice breaker activity",
+        description: "Gives some time for participants to introduce themselves to one another - Activity",
+        guests: ["Participants", "MC"],
+        duration: "15",
+        start: "9:20:00 AM",
+        end: "9:35:00 AM"
+    },
+    {
+        event: "Keynote",
+        description: "Future of work in the tech space",
+        guests: ["Eng. Andrew Masila, Head of Innovation Safaricom"],
+        duration: "25",
+        start: "9:35:00 AM",
+        end: "10:00:00 AM"
+    },
+    {
+        event: "Break",
+        description: "Break",
+        guests: ["Break"],
+        duration: "45",
+        start: "10:00:00 AM",
+        end: "10:45:00 AM"
+    },
+    {
+        event: "Presentation",
+        description: "Future thinking",
+        guests: ["Think Learning Studio (TLS)"],
+        duration: "45",
+        start: "10:45:00 AM",
+        end: "11:30:00 AM"
+    },
+    {
+        event: "Presentation",
+        description: "Read to work",
+        guests: ["Absa bank, Kenya"],
+        duration: "20",
+        start: "11:30:00 AM",
+        end: "11:50:00 AM"
+    },
+    {
+        event: "Lunch",
+        description: "Lunch",
+        guests: ["Lunch"],
+        duration: "50",
+        start: "11:50:00 AM",
+        end: "12:40:00 PM"
+    },
+    {
+        event: "Workshop",
+        description: "Afribot - Micro:bit (participants split into breakout rooms for these two workshops)",
+        guests: ["micro:bit"],
+        duration: "75",
+        start: "12:40:00 PM",
+        end: "1:55:00 PM"
+    },
+    {
+        event: "Feedback collection",
+        description: "Feedback collection (qr code with link to forms)",
+        guests: ["Feedback collection (qr code with link to forms)"],
+        duration: "10",
+        start: "1:55:00 PM",
+        end: "2:05:00 PM"
+    },
+]
