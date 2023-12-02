0ade60e730f429a785fc9c4bf8d7dc64a7a46d11
codeme254 <otwomadennis365@gmail.com>
2023-12-02 15:15:37 +0300

    Table component and day 2 data
    Create a table component and add day 2 data both to the data faile and
    to the main schedule component

--- teens-innovation-week/src/pages/Schedule/Schedule.jsx
+++ teens-innovation-week/src/pages/Schedule/Schedule.jsx
@@ -1,11 +1,13 @@
 import { useMemo } from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-import { day1 } from "./scheduleData";
+import { day1, day2 } from "./scheduleData";
 import { useTable } from "react-table";
+import Table from "./Table";
 
 const Schedule = () => {
   const data = useMemo(() => day1, []);
+  const dataDay2 = useMemo(() => day2, []);
   const columns = useMemo(
     () => [
       {
@@ -35,39 +37,22 @@ const Schedule = () => {
     ],
     [],
   );
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data });
+
   return (
     <div className="schedule">
       <Header />
       <h3 className="section-title u-marg-top-lg">Schedule</h3>
       <div className="table-container">
-        <h3 style={{ textAlign: "center" }}>Day 1</h3>
-        <table {...getTableProps()}>
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps()}>
-                    {column.render("Header")}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  ))}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <h3 style={{ textAlign: "center" }}>
+          5<sup>th</sup> December 2023
+        </h3>
+        <Table columns={columns} data={data} />
+      </div>
+      <div className="table-container">
+        <h3 style={{ textAlign: "center" }}>
+          6<sup>th</sup> December 2023
+        </h3>
+        <Table columns={columns} data={dataDay2} />
       </div>
       <Footer />
     </div>
